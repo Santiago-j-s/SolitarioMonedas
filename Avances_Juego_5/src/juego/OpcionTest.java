1da43d5b17514285b0bcb0331e008f88f4c06627
Santiago Santana <santana.santiago@gmail.com>
2016-07-29 18:18:20 -0300

    Mejoras al test de Pregunta

---
+++ Avances_Juego_5/src/juego/OpcionTest.java
@@ -0,0 +1,39 @@
+package juego;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OpcionTest {
+
+  @Test
+  public void testOpcionStringBoolean() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testOpcion() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetTituloOpcion() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testEsCorrecta() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testToString() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testEqualsObject() {
+    fail("Not yet implemented");
+  }
+
+}
